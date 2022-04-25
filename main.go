4985b7ec0ba6b6189c52703250c49f8522a4fecb
Michael Campitiello <michaelrcampi@gmail.com>
2022-04-25 10:27:44 -0700

    Structs

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-func main() {
+import "time"
 
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
 }
 
-func saySomething(s string) (string, string) {
-	return s, "World"
+func main() {
+
 }
