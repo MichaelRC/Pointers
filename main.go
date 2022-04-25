03a621ba361a54fee549d240189eca91a2755cfc
Michael Campitiello <michaelrcampi@gmail.com>
2022-04-25 10:07:28 -0700

    Next Lesson

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
-import "log"
-
 func main() {
-	myString := "Green"
 
-	log.Println("myString is set to", myString)
-	changeUSingPointer(&myString)
-	log.Panicln("after func call myString is set to", myString)
 }
 
-func changeUSingPointer(s *string) {
-	log.Println("s is set to", s)
-	newValue := "Red"
-	*s = newValue
+func saySomething(s string) (string, string) {
+	return s, "World"
 }
